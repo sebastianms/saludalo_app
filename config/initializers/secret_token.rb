f5ae20b24dc0bac5cd44c6455f11e0f8cce652aa
Sebastián Alonso Machuca Sáez <sebastianms@wf-iuc-sj-b-85-128.uc.cl>
2012-12-01 16:19:27 -0300

    Primer Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Saludalo::Application.config.secret_token = '3cbd149cd039b9fb15d5e9bb9f86a2441a3097863c383330b5dbbfde3e2dc37f73124228766d0f51d99cdeb44b851b9cebff1b07a04208d8a8e427cc75867274'
