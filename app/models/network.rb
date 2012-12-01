6c0e6497ba3fd6383c88c358e246a329fd428701
Tomás Barrios <tomasbarrios@gmail.com>
2012-12-01 18:24:36 -0300

    relaciones entre user y network

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,5 @@
 class Network < ActiveRecord::Base
+  has_many :user_networks
+  has_many :users, :through => :user_networks
   # attr_accessible :title, :body
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :user_networks
+  has_many :users, :through => :user_networks
   attr_accessible :email, :name, :password
 end
