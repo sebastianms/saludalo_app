b23492749fa2d44800497c4c2bfb6c35bb1ba1d8
Sebastian Alonso Machuca <sams91@gmail.com>
2012-12-01 17:03:05 -0300

    se borra index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  #before_filter :auth_user_redirect
+
+  def auth_user_redirect
+    redirect_to root_path if session[:user_id]
+  end
 
   def login
     user = User.find_by_email(params[:email])
@@ -8,6 +13,11 @@ class ApplicationController < ActionController::Base
     else
       redirect_to root_path
     end
+  end
+
+  def logout
+    session[:user_id] = nil
+    redirect_to root_path
   end
 
 end
