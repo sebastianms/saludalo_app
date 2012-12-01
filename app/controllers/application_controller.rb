1bbdabcc8d00b08b574aa8c4d869e3b34474979d
Sebastian Alonso Machuca <sams91@gmail.com>
2012-12-01 17:13:57 -0300

    Se crea controlador Public

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,4 +20,16 @@ class ApplicationController < ActionController::Base
     redirect_to root_path
   end
 
+  helper_method :current_user
+
+  def current_user
+    return nil if session[:user_id].nil?
+    user = User.find(session[:user_id])
+    if user
+      return user
+    else
+      return nil
+    end
+  end
+
 end
