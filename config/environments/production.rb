15eef72d50904eaa70884b5031969453fe33e911
Sebastian Alonso Machuca <sams91@gmail.com>
2012-12-02 18:25:24 -0300

    precompiling heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,6 +55,7 @@ Mejoramigo::Application.configure do
 
   # Enable threaded mode
   # config.threadsafe!
+  config.assets.enabled = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
