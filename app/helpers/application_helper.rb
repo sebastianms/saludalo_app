b5d7d1d7321001e3c01b9fa104021102d7da0cc8
Sebastian Alonso Machuca <sams91@gmail.com>
2012-12-02 11:47:24 -0300

    se muestra la calificacionde la tarea

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,47 @@ module ApplicationHelper
         return "Completada"
       end
     end
+
+    def califications_icons(mark)
+      case mark
+      when 1
+        html = <<-HTML
+            <i class="icon-star-empty"></i>
+        HTML
+        html.html_safe
+      when 2
+        html = <<-HTML
+            <i class="icon-star-empty"></i>
+            <i class="icon-star-empty"></i>
+        HTML
+        html.html_safe
+
+      when 3
+        html = <<-HTML
+            <i class="icon-star-empty"></i>
+            <i class="icon-star-empty"></i>
+            <i class="icon-star-empty"></i>
+        HTML
+        html.html_safe
+
+      when 4
+        html = <<-HTML
+            <i class="icon-star-empty"></i>
+            <i class="icon-star-empty"></i>
+            <i class="icon-star-empty"></i>
+            <i class="icon-star-empty"></i>
+        HTML
+        html.html_safe
+
+      when 5
+        html = <<-HTML
+            <i class="icon-star-empty"></i>
+            <i class="icon-star-empty"></i>
+            <i class="icon-star-empty"></i>
+            <i class="icon-star-empty"></i>
+            <i class="icon-star-empty"></i>
+        HTML
+        html.html_safe
+      end
+    end
 end
