1b310c78a53e08122252f9b938f14e532e6e3068
Jimi <jimi@jimi.(none)>
2012-12-01 22:30:01 -0300

    Corrección relación user task

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,7 @@
 class User < ActiveRecord::Base
   has_many :user_networks
   has_many :users, :through => :user_networks
-
-
-
-  has_one :task
+  has_many :task
 
 
   attr_accessible :email, :name, :password
