09c9d60acf65c42f78332d3b35d2e85a009229fe
Sebastian Alonso Machuca <sams91@gmail.com>
2012-12-02 11:13:52 -0300

    creacion de helper para estado de tarea

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,13 @@ module ApplicationHelper
 	def show_sessions
 		"CAUSE_ID => #{session[:cause_id]}"
 	end
+
+    def translate_status (status)
+      case status
+      when "pending"
+        return "Pendiente"
+      else
+        return "Completada"
+      end
+    end
 end
